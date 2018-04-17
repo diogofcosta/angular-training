6558cf2133f6b55cc714866c220637e951743a82
Diogo Filipe Costa <diogo.filip.costa@gmail.com>
2018-04-17 15:08:37 +0100

    playing around with different types of databinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    serverID = 10; // could also strong type it using serverID: Number = 10; but it's automatically infered by TS
+    serverStatus = 'offline'; // same here but with string
 
+    // method to get serverStatus property
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: 'app-servers', //as a normal component
-  // selector: '[app-servers]', // as a css attr selector
-  selector: '.app-servers', // as a css class selector
+  selector: 'app-servers', // as a normal component
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationMessage = 'No server was created yet!';
+  serverName = 'Placeholder';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } , 3000);
+  }
 
   ngOnInit() {
+  }
+
+  // creating a server handler (reacting to an event)
+  onCreateServer() {
+    this.serverCreationMessage = `A server was created! Name is ${this.serverName}`;
+  }
+
+  onUpdateServerName(event: Event) {
+    // console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
