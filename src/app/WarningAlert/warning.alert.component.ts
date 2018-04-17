cd79ce8cbb5c1d6685df1bab53325ae4cf0aaa4d
Diogo Filipe Costa <diogo.filip.costa@gmail.com>
2018-04-17 12:16:42 +0100

    finished assignment

---
+++ src/app/WarningAlert/warning.alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/**
+ * This was manually created!
+ */
+
+@Component({
+    selector: 'app-warning-alert',
+    styleUrls: ['./warning.alert.component.css'],
+    templateUrl: './warning.alert.component.html'
+})
+export class WarningAlertComponent {
+
+}
---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * This was generated with 'ng generate component'
+ */
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
