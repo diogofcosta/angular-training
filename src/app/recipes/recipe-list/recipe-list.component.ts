5a3b61ce4f10546e53cd926b004b5c18f5536b19
diogofcosta <diogo.filip.costa@gmail.com>
2018-04-18 17:09:57 +0100

    added a few recipes to the list hardcoded for now

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [  // signaling TS that this array will only ever store objects of type Recipe (our model)
+    new Recipe('Bacon and Eggs', 'Delicious combination of flavours!', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'),
+    new Recipe('Smoked Ribs', 'Heavenly food!', 'https://c1.staticflickr.com/3/2584/3978168591_177183e515_z.jpg')
+  ];
 
   constructor() { }
 
