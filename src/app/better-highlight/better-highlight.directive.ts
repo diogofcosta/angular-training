706110c20c2492278537b664406d12a6028dad48
diogofcosta <diogo.filip.costa@gmail.com>
2018-04-20 17:48:46 +0100

    stuff

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,27 +1,30 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit{
+export class BetterHighlightDirective implements OnInit {
+
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'red';
 
   @HostBinding('style.color') fontColor: string = 'initial';
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
-    // this.renderer.setStyle(this.elementRef.nativeElement , 'background-color', 'red');
+    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color', this.defaultColor);
   }
 
   @HostListener('mouseenter') onMouseEnter(eventData: Event) {
     // now it will only style it red on mouseenter events on the HTML element we use the directive on
-    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color', 'red');
+    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color', this.highlightColor);
     this.fontColor = 'white';
   }
 
   @HostListener('mouseleave') onMouseLeave(eventData: Event) {
     // now it will only style it red on mouseenter events on the HTML element we use the directive on
-    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color', 'transparent');
+    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color', this.defaultColor);
     this.fontColor = 'black'; // instead of using the Rendered we can use HostBinding to bind to a DOM element property!
   }
 
