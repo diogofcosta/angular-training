c44b82966c688855a1375ee8bc09723e518b7a9e
diogofcosta <diogo.filip.costa@gmail.com>
2018-04-20 17:12:44 +0100

    better way to style element with directives

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,14 @@
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color', 'red');
+  }
+
+}
