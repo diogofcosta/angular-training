e953bcaa7d062ef9eb19b07b3ebe167cdedeb7f2
diogofcosta <diogo.filip.costa@gmail.com>
2018-04-24 17:57:21 +0100

    created a page not found component and added two new routes... a not-found route to show the not found component and a catch all route to catch all other route inputs and redirect them to not-found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
@@ -43,8 +44,12 @@ const appRoutes: Routes = [
     ]
   },
   {
-    path: '*',
-    component: HomeComponent
+    path: 'not-found',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
   }
 ];
 
@@ -56,7 +61,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
